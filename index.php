85e74abb99ae0fc024101d0f9886d9a19e50ab6b
rosenclosed <rose@rosedev.de>
2024-06-08 11:10:54 +0200

    adds html structure

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Klangmemory</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="record-section">
+        <h2>Nehmen Sie Ihre Töne auf</h2>
+        <div id="recordings-list"></div>
+        <button id="start-recording">Aufnahme starten</button>
+        <button id="stop-recording" disabled>Aufnahme stoppen</button>
+        <button id="start-game" disabled>Spiel starten</button>
+    </div>
+    <div id="game-board" style="display: none;">
+        <!-- Die Karten werden hier dynamisch eingefügt -->
+    </div>
+    <script src="js/script.js"></script>
+    <script>
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+</script>
+
+</body>
+</html>
