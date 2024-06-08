750f5967b4d9d56be5bcceb1552cecfe7345574a
rosenclosed <rose@rosedev.de>
2024-06-08 12:28:40 +0200

    deploys manifest

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ header("Expires: 0"); // Proxies
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
+    <link rel="manifest" href="manifest.json" />
 </head>
 <body>
     <div id="record-section">
